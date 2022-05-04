c53a89fe2b66c93fb1d631c47108722da71fb40e
Joe Vieira <38537834+ProfJoeVieira@users.noreply.github.com>
2022-05-04 01:26:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token ='5158191525:AAHtxyO_hpIz8KF8T5JZB9HRZuemAMoQxH8';
+const token ='BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
